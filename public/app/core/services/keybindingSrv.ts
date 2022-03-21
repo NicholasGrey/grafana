4e6afbcc890a791e69b67376873d2cd326f2c402
Nicholas Greyling <nicholas.gr@gmail.com>
2022-03-21 10:39:34 +0200

    Update keybindingSrv.ts

--- public/app/core/services/keybindingSrv.ts
+++ public/app/core/services/keybindingSrv.ts
@@ -37,14 +37,15 @@ export class KeybindingSrv {
 
   initGlobals() {
     if (locationService.getLocation().pathname !== '/login') {
-      this.bind(['?', 'h'], this.showHelpModal);
-      this.bind('g h', this.goToHome);
-      this.bind('g a', this.openAlerting);
-      this.bind('g p', this.goToProfile);
-      this.bind('s o', this.openSearch);
-      this.bind('f', this.openSearch);
-      this.bind('esc', this.exit);
-      this.bindGlobal('esc', this.globalEsc);
+       return;
+      //this.bind(['?', 'h'], this.showHelpModal);
+      //this.bind('g h', this.goToHome);
+      //this.bind('g a', this.openAlerting);
+      //this.bind('g p', this.goToProfile);
+      //this.bind('s o', this.openSearch);
+      //this.bind('f', this.openSearch);
+      //this.bind('esc', this.exit);
+      //this.bindGlobal('esc', this.globalEsc);
     }
 
     this.bind('t t', () => toggleTheme(false));
